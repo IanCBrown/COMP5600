ff3c15624e36bef85acfb89800f414321b1c4b16
tStewart95 <tms0034@auburn.edu>
2019-11-11 17:43:47 -0600

    Changed preprocessing to output 2d array to file

--- Project/preprocessing.py
+++ Project/preprocessing.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+#takes 3d array as input and converts to a 2d array in black/white
+
 def main(inputArray):
-    processedImage = inputArray.copy()
-    for i in range(10,len(inputArray-10)):
+    processedImage = np.zeros((int(len(inputArray)-10),int(len(inputArray[0])-13)))
+    for i in range(10,len(inputArray)-10):
         for j in range(13,len(inputArray[i])-13):
             
             r = int(inputArray[i][j][0])
@@ -11,12 +13,12 @@ def main(inputArray):
 
             rgb = r + g + b
 
-          
             if(rgb < 325):
-              processedImage[i][j] = [0,0,0]
+              processedImage[i][j] = 0
             else:
-              processedImage[i][j] = [255,255,255]
+              processedImage[i][j] = 255
 
+            processedImage = processedImage
 
     np.save("imageData",processedImage)
 #    np.save("imageData",inputArray)
